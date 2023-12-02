7aa6c37bab85fb88ad02dbcb4227b315268190b9
minhquan <quandmph44109@fpt.edu.vn>
2023-12-02 22:56:19 +0700

    Delete da1 directory

--- da1/app/views/admin/header/header.php
+++
@@ -1,4 +0,0 @@
-<?php
-  require_once __DIR__ . "./link.php";
-  require_once __DIR__ . "./menu.php";
-?>
--- da1/app/views/client/footer/footer.php
+++
@@ -1,4 +0,0 @@
-<?php
-    require_once __DIR__ . "./menu.php";
-    require_once __DIR__ . "./link.php";
-?>
