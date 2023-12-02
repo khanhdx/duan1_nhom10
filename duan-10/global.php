e3245b18672b6edc67f10b6d167c17e41c8f691b
minhquan <quandmph44109@fpt.edu.vn>
2023-12-02 22:56:30 +0700

    Delete duan-10 directory

--- duan-10/global.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Chỉ cần khai báo 1 lần duy nhất tại đây
-session_start();
-
-const DB_HOST = 'localhost';
-const DB_DATABASE = 'duan_nhom10';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = '';
