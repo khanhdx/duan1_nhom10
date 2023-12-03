b92cc6683e47946b55eb602a1a9bbd71803e6334
minhquan <quan140504@gmail.com>
2023-12-03 12:07:35 +0700

    update

---
+++ global.php
@@ -0,0 +1,18 @@
+<?php
+
+// Chỉ cần khai báo 1 lần duy nhất tại đây
+session_start();
+
+const DB_HOST = 'localhost';
+const DB_DATABASE = 'duan_nhom10';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+
+if (!function_exists('check_auth')) {
+    function check_auth() {
+        if (empty($_SESSION['user'])) {
+            header('Location: /login');
+            exit();
+        }
+    }
+}
